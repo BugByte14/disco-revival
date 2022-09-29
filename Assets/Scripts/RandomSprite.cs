72e6b70ff770269d42aaa37892f00285702f7c59
William <wmr_1014@yahoo.com>
2022-09-28 20:16:09 -0500

    Init commit

---
+++ Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public Sprite[] sprite;
+    public int spriteAmount;
+
+    Random rand;
+    SpriteRenderer spriteRen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRen = GetComponent<SpriteRenderer>();
+
+        int rand = Random.Range(0, spriteAmount);
+
+        Debug.Log(rand);
+        spriteRen.sprite = sprite[rand];
+    }
+}
